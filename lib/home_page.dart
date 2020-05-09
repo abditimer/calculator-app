7426fd5c628389ee534ea2ae399114891b03d2c6
abditimer <thatimer@gmail.com>
2020-05-09 11:07:29 +0100

    app iteration 1 done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,10 +2,59 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  HomePageState createState() => HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class HomePageState extends State<HomePage> {
+  var num1 = 0, num2 = 0, sum = 0;
+
+  final TextEditingController t1 = new TextEditingController(text: "0");
+  final TextEditingController t2 = new TextEditingController(text: "0");
+
+  void doAddition() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+
+      sum = num1 + num2;
+    });
+  }
+
+  void doSub() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+
+      sum = num1 - num2;
+    });
+  }
+
+  void doMul() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+
+      sum = num1 * num2;
+    });
+  }
+
+  void doDiv() {
+    setState(() {
+      num1 = int.parse(t1.text);
+      num2 = int.parse(t2.text);
+
+      sum = num1 ~/ num2;
+    });
+  }
+
+  void doClear() {
+    setState(() {
+      t1.text="0";
+      t2.text = "0";
+      sum=0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -15,25 +64,59 @@ class _HomePageState extends State<HomePage> {
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new TextField(
-              keyboardType: TextInputType.number,
-              decoration: new InputDecoration(
-                hintText: "Enter no 1"
-              ),
+            new Text(
+              "Output : $sum",
+              style: new TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.purple),
             ),
             new TextField(
               keyboardType: TextInputType.number,
-              decoration: new InputDecoration(
-                hintText: "Enter no 1"
-              ),
+              decoration: new InputDecoration(hintText: "Enter no 1"),
+              controller: t1,
+            ),
+            new TextField(
+              keyboardType: TextInputType.number,
+              decoration: new InputDecoration(hintText: "Enter no 2"),
+              controller: t2,
             ),
             new Padding(padding: const EdgeInsets.only(top: 20.0)),
             new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new MaterialButton(
+                    child: new Text("+"),
+                    color: Colors.greenAccent,
+                    onPressed: doAddition),
+                new MaterialButton(
+                    child: new Text("-"),
+                    color: Colors.greenAccent,
+                    onPressed: doSub),
+              ],
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new MaterialButton(
+                  child: new Text("*"),
+                  color: Colors.greenAccent,
+                  onPressed: doMul,
+                ),
+                new MaterialButton(
+                    child: new Text("/"),
+                    color: Colors.greenAccent,
+                    onPressed: doDiv)
+              ],
+            ),
+            new Padding(padding: const EdgeInsets.only(top: 10.0)),
+            new Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                new RaisedButton(
-                  child: new Text("Add"),
-                  onPressed: () {}
+                new MaterialButton(
+                  child: new Text("Clear"),
+                  color: Colors.greenAccent,
+                  onPressed: doClear,
                 ),
               ],
             ),
